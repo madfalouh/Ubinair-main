6bd5c2d53f65898fda76ba9ec564edea7e944b72
madfalouh <70645288+madfalouh@users.noreply.github.com>
2022-08-15 18:46:50 +0300

    test ci/cd

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,7 +2,7 @@ import React from 'react'
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-
+import { ThHTMLAttributes } from 'react'
 import { Provider } from 'react-redux'
 import { combineReducers } from 'redux'
 import {
